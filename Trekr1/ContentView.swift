afbf0946a5b412d3bb7c323d8db6a769ccd99f5d
Bekhruz Kattaev <Bkattaev@yahoo.com>
2020-12-23 16:28:06 -0800

    Initial Commit

---
+++ Trekr1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trekr1
+//
+//  Created by Bekhruz Kattaev on 12/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trekr1/Trekr1App.swift
@@ -0,0 +1,17 @@
+//
+//  Trekr1App.swift
+//  Trekr1
+//
+//  Created by Bekhruz Kattaev on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct Trekr1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
